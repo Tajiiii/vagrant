4419699c3beef3926a920ab300464d374958e3eb
Tajiiii <daisuke.tajima.0218@gmail.com>
2020-08-06 17:30:38 +0900

    [Finish 3-6]

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -13,7 +13,15 @@ class TodolistsController < ApplicationController
   	list.save
 
   	#3.　トップ画面へリダイレクト
-  	redirect_to '/top'
+  	redirect_to todolist_path(list.id)
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
   end
 
   private
